6b05279ec556accd4d3b0d4edce17d124f14ef9e
niharikakarnik <niharika.karnik@gmail.com>
2023-05-07 15:26:42 -0700

    Alpine module

--- niharika-website/src/App.js
+++ niharika-website/src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
+import typewriter from '@marcreichel/alpine-typewriter';
+import Alpine from 'alpinejs';
+
+Alpine.plugin(typewriter);
+Alpine.start();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-            Niharika Website 
-        </p>
+        <span x-data="{ texts: ['Welcome to my website', 'Good to see you here'] }" x-typewriter="texts"></span>
       </header>
     </div>
   );
